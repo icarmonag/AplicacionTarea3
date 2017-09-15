e0f1f5203627af04ea2ed054e25964e1844652a6
icarmonag <31824460+icarmonag@users.noreply.github.com>
2017-09-15 09:11:32 -0500

    Initial Commit

---
+++ AplicacionTarea3/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AplicacionTarea3
+//
+//  Created by Ivan on 9/15/17.
+//  Copyright © 2017 wondererApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ AplicacionTarea3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AplicacionTarea3
+//
+//  Created by Ivan on 9/15/17.
+//  Copyright © 2017 wondererApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
