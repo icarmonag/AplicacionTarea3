ff5df9b3c411bfc59c9e66f49680cfd40efd4aa7
icarmonag <31824460+icarmonag@users.noreply.github.com>
2017-09-15 18:52:31 -0500

    Tarea 3 completa

--- AplicacionTarea3/Home.m
+++ AplicacionTarea3/Home.m
@@ -18,6 +18,9 @@ CGFloat red=0.00;
 CGFloat blue=0.00;
 CGFloat green=0.00;
 CAShapeLayer *circleLayer;
+NSString *alertTxt;
+NSString *hex;
+BOOL drawing=true;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -31,30 +34,45 @@ CAShapeLayer *circleLayer;
 //[redSlider addTarget:self action:@selector(changeRedValue:) forControlEvents:UIControlEventValueChanged];
 
 - (IBAction)changeRedValue:(id)sender {
+    
     red=self.redSlider.value;
     self.lblRed.text= [ NSString stringWithFormat:@"%.0f" ,red ];
+    if (drawing) {
     [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+
+    }
 }
 
 - (IBAction)changeGreenValue:(id)sender {
+    
     green=self.greenSlider.value ;
     self.lblGreen.text= [ NSString stringWithFormat:@"%.0f" ,green ];
+    if (drawing) {
     [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+
+    }
 }
 
 - (IBAction)changeBlueValue:(id)sender {
+    
     blue=self.blueSlider.value;
     self.lblBlue.text= [ NSString stringWithFormat:@"%.0f" ,blue ];
+    if (drawing) {
     [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
-    
+    }
 }
 - (IBAction)circleVisible:(id)sender {
     if (self.cirlceSwitch.isOn){
-        [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];    }
+        [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+        drawing=true;
+    }
     else{
-        [circleLayer setFillColor:[[UIColor clearColor] CGColor]];    }
+        [circleLayer setFillColor:[[UIColor clearColor] CGColor]];
+        drawing = false;
+    }
 }
 - (IBAction)randomColoredCircle:(id)sender {
+    if (drawing) {
     red = ((float)rand()/RAND_MAX)*255;
     green = ((float)rand()/RAND_MAX)*255;
     blue = ((float)rand()/RAND_MAX)*255;
@@ -65,9 +83,23 @@ CAShapeLayer *circleLayer;
     self.lblRed.text= [ NSString stringWithFormat:@"%.0f" ,red ];
     self.lblGreen.text= [ NSString stringWithFormat:@"%.0f" ,green ];
     self.lblBlue.text= [ NSString stringWithFormat:@"%.0f" ,blue ];
+    }
 }
 - (IBAction)alertMessage:(id)sender {
-    
+    if (drawing) {
+        hex = [NSString stringWithFormat:@"Color #%02x%02x%02x",
+           (unsigned int)[self.lblRed.text integerValue],
+    (unsigned int)[self.lblGreen.text integerValue],
+    (unsigned int)[self.lblBlue.text integerValue]];
+    }
+    else{
+        hex = @"No hay circulo";
+    }
+        
+    alertTxt = [ NSString stringWithFormat:@"Nombre: %@, Telefono: %@ . %@" ,self.labelNombre.text, self.labelTelefono.text, hex ];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Alert" message:alertTxt preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
