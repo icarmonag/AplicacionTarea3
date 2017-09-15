79ca174e3e748c002331efce3e40c77058b4dd46
icarmonag <31824460+icarmonag@users.noreply.github.com>
2017-09-15 12:04:13 -0500

    Agregados sliders, botón de la suerte, switch y text fields. Falta agregar alert view

--- AplicacionTarea3/Home.h
+++ AplicacionTarea3/Home.h
@@ -9,6 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface Home : UIViewController
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UILabel *lblRed;
+@property (weak, nonatomic) IBOutlet UILabel *lblGreen;
+@property (weak, nonatomic) IBOutlet UILabel *lblBlue;
+@property (weak, nonatomic) IBOutlet UISwitch *cirlceSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *luckyButton;
+@property (weak, nonatomic) IBOutlet UITextField *labelNombre;
+@property (weak, nonatomic) IBOutlet UITextField *labelTelefono;
 
 
 @end
--- AplicacionTarea3/Home.m
+++ AplicacionTarea3/Home.m
@@ -14,12 +14,59 @@
 
 @implementation Home
 
+CGFloat red=0.00;
+CGFloat blue=0.00;
+CGFloat green=0.00;
+CAShapeLayer *circleLayer;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    CAShapeLayer *circleLayer = [CAShapeLayer layer];
+    circleLayer = [CAShapeLayer layer];
     [circleLayer setPath:[[UIBezierPath bezierPathWithOvalInRect:CGRectMake(160, 50, 100, 100)] CGPath]];
     [[self.view layer] addSublayer:circleLayer];
+    //[circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+}
+
+//[redSlider addTarget:self action:@selector(changeRedValue:) forControlEvents:UIControlEventValueChanged];
+
+- (IBAction)changeRedValue:(id)sender {
+    red=self.redSlider.value;
+    self.lblRed.text= [ NSString stringWithFormat:@"%.0f" ,red ];
+    [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+}
+
+- (IBAction)changeGreenValue:(id)sender {
+    green=self.greenSlider.value ;
+    self.lblGreen.text= [ NSString stringWithFormat:@"%.0f" ,green ];
+    [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+}
+
+- (IBAction)changeBlueValue:(id)sender {
+    blue=self.blueSlider.value;
+    self.lblBlue.text= [ NSString stringWithFormat:@"%.0f" ,blue ];
+    [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+    
+}
+- (IBAction)circleVisible:(id)sender {
+    if (self.cirlceSwitch.isOn){
+        [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];    }
+    else{
+        [circleLayer setFillColor:[[UIColor clearColor] CGColor]];    }
+}
+- (IBAction)randomColoredCircle:(id)sender {
+    red = ((float)rand()/RAND_MAX)*255;
+    green = ((float)rand()/RAND_MAX)*255;
+    blue = ((float)rand()/RAND_MAX)*255;
+    [circleLayer setFillColor:[[UIColor colorWithRed: red/255.f green:green/255.f blue: blue/255.f alpha: 1.0] CGColor]];
+    self.redSlider.value = red;
+    self.greenSlider.value = green;
+    self.blueSlider.value = blue;
+    self.lblRed.text= [ NSString stringWithFormat:@"%.0f" ,red ];
+    self.lblGreen.text= [ NSString stringWithFormat:@"%.0f" ,green ];
+    self.lblBlue.text= [ NSString stringWithFormat:@"%.0f" ,blue ];
+}
+- (IBAction)alertMessage:(id)sender {
     
 }
 
